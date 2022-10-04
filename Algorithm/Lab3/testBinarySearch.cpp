53ab9bd9a9a2c4d8305593773773204611106a99
Erdaulet <erdaulet0341@gmail.com>
2022-10-04 11:03:48 +0600

    kbtu

--- Algorithm/Lab3/testBinarySearch.cpp
+++ Algorithm/Lab3/testBinarySearch.cpp
@@ -4,25 +4,11 @@ using namespace std;
 
 int main(){
     int n; cin >> n;
-    vector<int> v;
+    int arr[n];
     for (int i=0; i<n; i++){
-        int k; cin >> k;
-        v.push_back(k);
+        cin >> arr[i];
     }
-    int k; cin >> k;
-    sort(v.begin(), v.end());
-    int first=0, last = v.size()-1, middle;
-    while (first<=last){
-        middle = first + (last - first)/2;
-        if(v[middle] > k){
-            last = middle - 1;
-        }
-        else if(v[middle] < k){
-            first = middle +1;
-        }
-        else{
-            cout << middle;
-            break;
-        }
-    }
+    int* max = max_element(arr, arr+n);
+    int m = *max;
+    cout << m;
 }
